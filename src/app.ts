28a5a47855cb0bc285f11706641433989a805328
miwagrr <mariabsudre@gmail.com>
2025-09-28 17:51:14 -0300

    Corrigido erro dos routers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-export const app = express()
+export const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+// O app.listen() inicia o servidor, e está perfeito aqui.
 app.listen(3003, () => {
-    console.log('Servidor rodando na porta 3003')
-})
+    console.log('Servidor rodando na porta 3003');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import express from 'express'
-import cors from 'cors'
+import { app } from "./app"; 
+import { userRouter } from "./routes/UserRouter"; 
+import { postRouter } from "./routes/PostRouter"; 
 
-export const app = express()
+app.use('/users', userRouter); 
 
-app.use(express.json())
-app.use(cors())
-
-app.listen(3003, () => {
-    console.log('Servidor rodando na porta 3003')
-})
+app.use('/posts', postRouter);
